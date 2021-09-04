d99982f08361603b8a734dcc62dd71dba1a6e8b3
Alexander Rojas <alexrrojas.b@gmail.com>
2021-09-04 16:24:36 -0600

    lab5 finished

---
+++ lab5/lab5/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  lab5
+//
+//  Created by Alexander Rojas Benavides on 9/4/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ lab5/lab5/lab5App.swift
@@ -0,0 +1,17 @@
+//
+//  lab5App.swift
+//  lab5
+//
+//  Created by Alexander Rojas Benavides on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct lab5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
